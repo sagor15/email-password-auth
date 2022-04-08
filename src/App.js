18e0320536bad065b47a7ad04c468e312da31675
sagor20202021 <sagorahammed1562@gamil.com>
2022-04-08 14:39:09 +0600

    added a state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,68 @@
 import "./App.css";
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import app from "./firebase.init";
+// import { Button, Form } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import { useState } from "react";
 
 const auth = getAuth(app);
+
 function App() {
-  return <div className="App"></div>;
+  const [email, setEamil] = useState("");
+  const [password, setPassword] = useState("");
+  const [validated, setValidated] = useState(false);
+
+  const handleEmailBure = (e) => {
+    setEamil(e.target.value);
+  };
+  const handlePasswordBlur = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    setValidated(true);
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <div className="registation w-50 mx-auto mt-4">
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control onBlur={handleEmailBure} type="email" placeholder="Enter email" required />
+            <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+            <Form.Control.Feedback type="invalid">Please provide a valid email.</Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
+            <Form.Control.Feedback type="invalid">Please provide a valid password.</Form.Control.Feedback>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
